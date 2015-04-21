7e43b89da7ef6e464fd5b29566d8ab33afb99f3c
Josh Nagel <jnagel33@gmail.com>
2015-04-20 23:37:55 -0700

    Fixed webview rotation

--- GitHubToGo/UserRepoWebViewController.swift
+++ GitHubToGo/UserRepoWebViewController.swift
@@ -21,6 +21,16 @@ class UserRepoWebViewController: UIViewController {
     let urlRequest = NSURLRequest(URL: NSURL(string: self.repository.htmlURL)!)
     self.webView = WKWebView(frame: self.view.frame)
     self.view.addSubview(self.webView)
+    var top = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.Top, relatedBy: .Equal, toItem: self.view, attribute: NSLayoutAttribute.TopMargin, multiplier: 1.0, constant: 0)
+    self.view.addConstraint(top)
+    var bottom = NSLayoutConstraint(item: self.webView, attribute: NSLayoutAttribute.Bottom, relatedBy: .Equal, toItem:
+      self.view, attribute: NSLayoutAttribute.BottomMargin, multiplier: 1.0, constant: 0)
+    self.view.addConstraint(bottom)
+    var left = NSLayoutConstraint(item: self.webView, attribute: .LeadingMargin, relatedBy: .Equal, toItem: self.view, attribute: .Leading, multiplier: 1.0, constant: 0.0)
+    self.view.addConstraint(left)
+    var right = NSLayoutConstraint(item: self.webView, attribute: .TrailingMargin, relatedBy: .Equal, toItem: self.view, attribute: .Trailing, multiplier: 1.0, constant: 0)
+    self.view.addConstraint(right)
     self.webView.loadRequest(urlRequest)
+    self.webView.setTranslatesAutoresizingMaskIntoConstraints(false)
   }
 }
